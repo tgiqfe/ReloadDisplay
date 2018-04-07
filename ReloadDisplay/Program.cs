36bff4752d5f56ab456c3e1723c2a8d1ca0055bf
tgiqfe <tqji31001@gmail.com>
2018-04-07 15:00:48 +0900

    Init

---
+++ ReloadDisplay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Threading;
+
+namespace ReloadDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagementObject modis = new ManagementClass("Win32_PNPEntity").
+                GetInstances().
+                OfType<ManagementObject>().
+                FirstOrDefault(x => x["PNPClass"] != null && x["PNPClass"] as string == "Display");
+            modis.InvokeMethod("Disable", new object[] { 0 });
+            Thread.Sleep(100);
+            modis.InvokeMethod("Enable", new object[] { 0 });
+        }
+    }
+}
